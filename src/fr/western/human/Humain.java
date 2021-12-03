f98bc60928bd50b17212ed5e30d01d266c7385f9
Gio <georgesmessanges@hotmail.com>
2021-12-03 09:36:40 +0100

    first commit

---
+++ src/fr/western/human/Humain.java
@@ -0,0 +1,33 @@
+package fr.western.human;
+
+public class Humain {
+
+    private String nom;
+    private String boisson;
+
+    Humain(String nom){
+        this.nom= nom;
+        this.boisson = "eau";
+    }
+
+    public void parle(String texte){
+        System.out.println("("+this.nom+")" + " - " + texte);
+    }
+
+    public void sePresenter(){
+        System.out.println("Bonjour, je m'appelle "+this.nom+" et ma boisson favorite est : "+this.boisson);
+    }
+
+    public void boire(){
+        System.out.println("Ah ! un bon verre de "+this.boisson+ "! GLOUPS !");
+    }
+
+    public String quel_est_ton_nom(){
+       return this.nom;
+    }
+
+    public String boisson_favorite(){
+        return this.boisson;
+    }
+
+}
